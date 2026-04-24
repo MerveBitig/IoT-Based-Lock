b7cb2bf080d8f71e8e9179808d385d041a489c04
Sudenaz Merve Sıgırta <sudenazmerve.65@gmail.com>
2026-04-24 22:48:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import cv2
 import mediapipe as mp
 
-# En standart çağırma şekli
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture(0)
 
 with mp_face_detection.FaceDetection(min_detection_confidence=0.5) as face_detection:
-    print("Kamera aciliyor, lutfen bekleyin...")
+    print("Sistem baslatildi. Kamera aciliyor...")
     while cap.isOpened():
         success, image = cap.read()
-        if not success: break
+        if not success:
+            break
 
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = face_detection.process(image)
@@ -20,9 +20,9 @@ with mp_face_detection.FaceDetection(min_detection_confidence=0.5) as face_detec
         if results.detections:
             for detection in results.detections:
                 mp_drawing.draw_detection(image, detection)
-        
-        cv2.imshow('IoT Kilit Sistemi', image)
-        if cv2.waitKey(5) & 0xFF == ord('q'):
+
+        cv2.imshow('Kamera Test', image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 cap.release()
